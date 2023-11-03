42b0ee1a63d4f52963be45c105a68a7adfad56a9
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2023-11-02 19:03:38 -0700

    a program that prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 import importlib.util
 import sys
-
 if __name__ == "__main__":
     module_name = "hidden_4"
     module_spec = importlib.util.spec_from_file_location(module_name, "./hidden_4.pyc")
     module = importlib.util.module_from_spec(module_spec)
     module_spec.loader.exec_module(module)
-
     module_names = [name for name in dir(module) if not name.startswith("__")]
-
     module_names.sort()
-
     for name in module_names:
         print(name)
