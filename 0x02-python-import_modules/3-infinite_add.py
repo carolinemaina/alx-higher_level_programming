0ca7af3b6314c932603b2ceb0af85f9fd617066a
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2023-11-02 18:31:32 -0700

    3. Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    arguments = sys.argv[1:]
-    result = sum(map(int, arguments))
-    print(result)
+    args = sys.argv[1:]
+    n = sum(map(int, args))
+    print(n)
