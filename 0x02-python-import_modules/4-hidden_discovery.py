225207cbbbf2f832cff6ddcd29e24af63d8cbbf0
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2023-11-07 01:10:25 -0800

    errors handling

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,8 @@ import importlib.util
 import sys
 if __name__ == "__main__":
     module_name = "hidden_4"
-    module_spec = importlib.util.spec_from_file_location(module_name, "./hidden_4.pyc")
+    module_spec = importlib.util.spec_from_file_location(module_name,
+            "./hidden_4.pyc")
     module = importlib.util.module_from_spec(module_spec)
     module_spec.loader.exec_module(module)
     module_names = [name for name in dir(module) if not name.startswith("__")]
