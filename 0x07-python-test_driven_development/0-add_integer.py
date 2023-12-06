1eeb5de3e1f63242963fb983a7c2cd8dc5a026f1
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2023-12-05 23:29:59 -0800

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    """Check if a and b are integers or floats"""
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
-    
-    """Cast a and b to integers if they are floats"""
-    a = int(a) if isinstance(a, float) else a
-    b = int(b) if isinstance(b, float) else b
-    
-    """Return the addition of a and b"""
-    return int(a + b)
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return (a + b)
